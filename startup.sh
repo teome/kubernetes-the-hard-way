3d3aa57a4b430d19cac319cce4ae38ed921a7e39
teome <domkelly1010@gmail.com>
2024-05-08 17:48:42 +0100

    Make apt-get install without confirmation

--- startup.sh
+++ startup.sh
@@ -10,7 +10,7 @@ else
     echo "Docker command is not available, installing"
     # Add Docker's official GPG key:
     sudo apt-get update
-    sudo apt-get install ca-certificates curl
+    sudo apt-get install -y ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -22,5 +22,5 @@ else
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
 
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
